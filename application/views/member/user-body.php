359b49dfdda82067d91983f6e5fed86be3ab3478
rolies <azheema19@hotmail.com>
2015-12-14 09:48:52 +0700

    Update member dashboard ui

--- application/views/member/user-body.php
+++ application/views/member/user-body.php
@@ -2,117 +2,121 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-4 member-area">
 			<img src="<?php echo base_url(); ?>template/img/user.jpg" width="80px" class="pull-left">
 			<div class="greeting">
 				<small>Welcome Back</small>
 				<p> Brolin </p>
 				<a href="#" class="btn btn-danger ">SELL </a>
 			</div>
+			<div class="my-selling">
+				<div class=" ticket">
+					<h5> My Ticket Selling</h5>
+				</div>
+				<div class="row">
+					<div class="col-md-5">
+						<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" class="pull-left img-responsive">
+					</div>
+					<div class="curr-sell-small">
+						<strong> Bon Jovi </strong>
+						<p class="harga"> IDR 120.000 </p>
+						<a href="#" class="btn btn-warning ">Reserved </a>
+						
+					</div>
+				</div>
+			</div>
+			<div class="my-reserve">
+				<div class="ticket">
+					<h5> My Ticket Reservation</h5>
+				</div>
+				<div class="row">
+					<div class="col-md-5 curr-res-small">
+						<h5> Trip ke Bromo </h5>
+						<p> IDR 120.000 </p>
+						<a href="#" class="btn btn-warning btn-sm">Cancel </a>
+					</div>
+					<div class="col-md-7">
+						<p> IDR 120.000 </p>
+					</div>
+				</div>
+			</div>
 		</div>
 				
-		<div class="col-md-8">
-			<nav class="navbar">
+		<div class="col-md-7 col-md-offset-1">
+			<nav class="navbar navbar-default" role = "navigation">
+				<div class="container-fluid">
+					<div class="row fix-menu">
+						<div class="navbar-header">
+			  				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+			  					<span class="sr-only">Toggle navigation</span>
+			  					<span class="icon-bar"></span>
+			  					<span class="icon-bar"></span>
+			  					<span class="icon-bar"></span>
+			  				</button>
+						<!-- Your Logo --> 
 
-					<ul class="nav navbar-nav">
-						<li><a href="#">Semua Post</a></li>
-						<li class="active"><a href="#">Semua Reservasi</a></li>
-						<li><a href="#">Notifikasi</a></li>		
-					</ul>
-			</nav>
-		</div>
-	</div><br><br>
-	<div class="row">
-		<div class="col-md-4">
-			<div class=" ticket">
-				<h4> My Ticket Selling</h4>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
-					<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" width="150px" class="pull-left">
+						</div>
+						<!-- Start Navigation -->
+			  			<div class="collapse navbar-collapse" id="collapse" role="navigation">
+				  			<ul class="nav navbar-nav">
+								<li class="active"><a href="#">Semua Post</a></li>
+								<li><a href="#">Semua Reservasi</a></li>
+								<li><a href="#">Notifikasi</a></li>	
+				  			</ul>
+						</div>
+					</div>		
 				</div>
-				<div class="col-md-6">
-					<h5> Bon Jovi </h5>
-					<p> IDR 120.000 </p>
-					<a href="#" class="btn btn-warning ">RESERVED </a>
-					
-				</div>
-			</div>
-		</div>
-				
-		<div class="col-md-8 centerRight">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="col-md-4">
-						<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" width="70px" class="pull-left">
-					</div>
-					<div class="col-md-8">
-						<h5> Bon Jovi </h5>
-						<p class="h8"> IDR 120.000 </p>
+		  	</nav>
+		  	<div class="active-ticket">
+		  		<div class="col-md-6 img-area">
+					<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" width="120px" class="pull-left img-responsive">
+					<div class="info-small">
+						<strong>Bon Jovi</strong>
+						<p class="harga"> IDR 120.000 </p>
 						<small> ndak ketok tulisannya bos </small>
-						<a href="#" class="btn btn-warning btn-xs">Reserved</a>
+						<a href="#" class="btn btn-warning">Reserved</a>
 					</div>
 				</div>
-				<div class="col-md-4 address">
+				<div class="col-md-3">
 					<p> Jl Imam Bonjol , Ciputat </p>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-3">
 					<a href="#" class="btn btn-info"> Edit </a>
 				</div>
 			</div>
-		</div>
-	</div><br><br>
-	<div class="row">
-		<div class="col-md-4">
-			<div class=" ticket">
-				<h4> My Ticket Reservation</h4>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
-					<h5> Trip ke Bromo </h5>
-					<p> IDR 120.000 </p>
-					<a href="#" class="btn btn-warning btn-sm">CANCEL </a>
-				</div>
-				<div class="col-md-6">
-					<p> IDR 120.000 </p>
-				</div>
-			</div>
-		</div>
-				
-		<div class="col-md-8 buttomRight">
-			<div class="row">
+
+			<div class="list-ticket">
+				<hr>
 				<div class="col-md-2">
-					<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" width="70px" class="pull-left">
+					<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" class="pull-left img-responsive">
 				</div>
-				<div class="col-md-10">
+				<div class="info-small">
 					<h5>Konser Iwan Fals </h5>
 					<small>IDR 120.000 sampingnya gk keliatan bos</small><br>
-					<a href="#" class="btn btn-success btn-xs ">Reserved</a>
+					<a href="#" class="btn btn-danger">Reserved</a>
 				</div>
-			</div>
-			<div class="row">
+				<hr>
 				<div class="col-md-2">
-					<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" width="70px" class="pull-left">
+					<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" class="pull-left img-responsive">
 				</div>
-				<div class="col-md-10">
+				<div class="info-small">
 					<h5> Trip Perjalanan ke Parang Tritis </h5>
 					<small>IDR 120.000 sampingnya gk keliatan bos</small><br>
-					<a href="#" class="btn btn-pink btn-xs ">Reserved</a>
+					<a href="#" class="btn btn-success">Reserved</a>
 				</div>
-			</div>
-			<div class="row">
+				<hr>
 				<div class="col-md-2">
-					<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" width="70px" class="pull-left">
+					<img src="<?php echo base_url(); ?>template/img/ticket1.jpg" class="pull-left img-responsive">
 				</div>
-				<div class="col-md-10">
+				<div class="info-small">
 					<h5> Trip Konser Owl City</h5>
 					<small>IDR 120.000 sampingnya gk keliatan bos</small><br>
 					<a href="#" class="btn btn-success btn-xs ">Reserved</a>
 				</div>
 			</div>
 		</div>
-	</div><br><br>
-	
-</div>
+	</div>
+</div>	
+
 
 
