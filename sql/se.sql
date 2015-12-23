ea8af57b2be5a73af79c714a74bfcde98478b9a8
rolies <azheema19@hotmail.com>
2015-12-23 20:42:15 +0700

    update sql

--- sql/se.sql
+++ sql/se.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2015 at 07:39 AM
+-- Generation Time: Dec 23, 2015 at 02:40 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -29,23 +29,27 @@ SET time_zone = "+00:00";
 CREATE TABLE `sellpost` (
   `number` int(11) NOT NULL,
   `tipe` varchar(20) NOT NULL,
-  `judul` varchar(40) NOT NULL,
-  `tanggal` varchar(10) NOT NULL,
+  `judul` varchar(160) NOT NULL,
+  `tanggal` datetime NOT NULL,
   `harga` double NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `deskripsi` varchar(200) NOT NULL,
-  `gambar` varchar(10) NOT NULL,
-  `status` varchar(10) NOT NULL
+  `gambar` varchar(60) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `user` varchar(12) NOT NULL,
+  `keterangan` varchar(160) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `sellpost`
 --
 
-INSERT INTO `sellpost` (`number`, `tipe`, `judul`, `tanggal`, `harga`, `alamat`, `deskripsi`, `gambar`, `status`) VALUES
-(1, 'Economy', 'Bon Jovi', 'Review', 200000, 'Jl Imam Bonjol , Ciputat, Jakarta', 'Tiket Nonton Bon Jovi live in Jakarta', '', 'active'),
-(2, 'Government', 'Tiket ke gilgit baltistan', 'Complain', 400000, '', '', '', 'deactived'),
-(3, 'Pariwisata', 'Tiket ke mount k2', 'Update', 4000000, 'Semarang', 'ini tiket untuk berangkat ke k2 harga murah terjangkau', '', 'reserved');
+INSERT INTO `sellpost` (`number`, `tipe`, `judul`, `tanggal`, `harga`, `alamat`, `deskripsi`, `gambar`, `status`, `user`, `keterangan`) VALUES
+(4, 'Perjalanan', 'Bon Jovi live Jakarta', '2015-09-10 00:00:00', 140000, 'Jakarta', 'savnakjsv kha svaivdais vaiscias ciasbcias caisbcias vaisbcias c aisdbcisab', 'ticket2.jpg', 'sold', 'brolin', ''),
+(5, 'Perjalanan', 'Bromo', '2015-09-12 10:30:00', 140000, 'Malang', 'savnakjsv kha svaivdais vaiscias ciasbcias caisbcias vaisbcias c aisdbcisab', 'ticket2.jpg', 'sold', 'brolin', ''),
+(6, 'Perjalanan', 'Tiket ke Bromo', '2015-10-04 07:35:00', 200000, 'Semarang', ' sgerb r sf sf s df sdv g', 'ticket2.jpg', 'deactived', 'brolin', 'post ini melanggar peraturan kami'),
+(7, 'Pariwisata', 'Tiket ke Dieg', '2016-03-10 05:25:00', 300000, 'Magelang', 'Jual ticket ke dieg magelang harga murah', 'ticket1.jpg', 'active', 'brolin', ''),
+(8, 'Perjalanan', 'Tiket ke Bromo', '2015-10-04 07:35:00', 200000, 'Semarang', ' sgerb r sf sf s df sdv g', 'ticket2.jpg', 'deactived', 'lambang', 'post ini melanggar peraturan kami');
 
 -- --------------------------------------------------------
 
@@ -96,7 +100,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `sellpost`
 --
 ALTER TABLE `sellpost`
-  MODIFY `number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
